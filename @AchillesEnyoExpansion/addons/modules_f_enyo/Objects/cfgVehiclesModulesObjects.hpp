5ad065fb0e22acccb31adfe08df8fcd2eacdbdb6
CreepPork <creepporkstudios@gmail.com>
2017-05-08 23:36:51 +0300

    Added the ECM module settings Moved the images to the Base Module

--- @AchillesEnyoExpansion/addons/modules_f_enyo/Objects/cfgVehiclesModulesObjects.hpp
+++ @AchillesEnyoExpansion/addons/modules_f_enyo/Objects/cfgVehiclesModulesObjects.hpp
@@ -1,8 +1,8 @@
 class Enyo_Objects_Module_Base : Enyo_Module_Base
 {
 	category = "Objects";
-	icon = "";
-	portrait = "";
+	icon = "\achilles\data_f_achilles\icons\icon_object.paa";
+	portrait = "\achilles\data_f_achilles\icons\icon_object.paa";
 };
 
 class Enyo_IED_Module : Enyo_Objects_Module_Base
@@ -10,8 +10,15 @@ class Enyo_IED_Module : Enyo_Objects_Module_Base
 	scopeCurator = 2;
 	curatorCanAttach = 1;
 	_generalMacro = "Enyo_IED_Module";
-	displayName = "Set IED";
+	displayName = "Create IED";
 	function = "Enyo_fnc_ObjectIED";
-	icon = "\achilles\data_f_achilles\icons\icon_object.paa";
-	portrait = "\achilles\data_f_achilles\icons\icon_object.paa";
+};
+
+class Enyo_AddECM_Module : Enyo_Objects_Module_Base
+{
+	scopeCurator = 2;
+	curatorCanAttach = 1;
+	_generalMacro = "Enyo_AddECM_Module";
+	displayName = "Add ECM to Vehicle";
+	function = "Enyo_fnc_ObjectAddECM";
 };
