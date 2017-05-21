3668894efcbd906c7420f1f4c9b1bcc6fbc4f560
CreepPork <creepporkstudios@gmail.com>
2017-05-21 16:00:43 +0300

    Added basic localization #14 (broken needs more work, returns empty strings)

---
+++ @AchillesEnyoExpansion/addons/language_f_enyo/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+  class enyo_language_f
+  {
+    weapons[] = {};
+    requiredVersion = 0.1;
+    author = "CreepPork_LV";
+    authorUrl = "https://github.com/CreepPork/AchillesEnyoExpansion";
+    version = 0.0.1;
+    versionStr = "0.0.1";
+    versionAr[] = {0,0,1};
+
+    units[] = {};
+    requiredAddons[] = {};
+  };
+};
