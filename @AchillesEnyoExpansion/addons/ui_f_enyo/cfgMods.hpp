29021a93d9276540769c640b0a3a4cfe6754a6e3
CreepPork <creepporkstudios@gmail.com>
2017-05-22 15:03:59 +0300

    Added DLC icon to show in Zeus #6

--- @AchillesEnyoExpansion/addons/ui_f_enyo/cfgMods.hpp
+++ @AchillesEnyoExpansion/addons/ui_f_enyo/cfgMods.hpp
@@ -1,4 +1,23 @@
 class CfgMods
 {
+  class Enyo
+  {
+    dir = "@AchillesEnyoExpansion";
+    name = "Enyo Expansion";
+    author = "CreepPork_LV";
+    hidePicture = 0;
+    hideName = 1;
 
+    actionName = "Website";
+    action = "https://forums.bistudio.com";
+
+    description = "Ares augments the existing Zeus functionality, expanding the toolset and making it easy to create more compelling missions on the fly.<br />The Achilles Expansion (AE) includes Ares Mod with even more functionalites. Some functionalities only work with ACE mod, but those mods are not mandatory in order to run Ares (AE) properly.<br/>The Enyo Expansion focuses more on bringing back the functionalites from MCC Sandbox 4 with improved support and them being updated to newest Arma 3 standards. It uses elements from Achilles Expansion addon that improve the look and feel of Enyo Expansion.";
+    
+    overview = "Ares augments the existing Zeus functionality, expanding the toolset and making it easy to create more compelling missions on the fly.<br />The Achilles Expansion (AE) includes Ares Mod with even more functionalites. Some functionalities only work with ACE mod, but those mods are not mandatory in order to run Ares (AE) properly.<br/>The Enyo Expansion focuses more on bringing back the functionalites from MCC Sandbox 4 with improved support and them being updated to newest Arma 3 standards. It uses elements from Achilles Expansion addon that improve the look and feel of Enyo Expansion.";
+
+    picture = "\enyo\data_f_enyo\icons\icon_enyo.paa";
+    logo = "\enyo\data_f_enyo\icons\icon_enyo.paa";
+    logoOver = "\enyo\data_f_enyo\icons\icon_enyo.paa";
+    logoSmall = "\enyo\data_f_enyo\icons\icon_enyo.paa";
+  };
 };
