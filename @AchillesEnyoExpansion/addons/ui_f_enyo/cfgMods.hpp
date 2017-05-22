63ddd0ef36b9820ef34b9559aa0da8c2eb8ac2a1
CreepPork <creepporkstudios@gmail.com>
2017-05-22 15:22:39 +0300

    Added larger Enyo icon Tweaked text

--- @AchillesEnyoExpansion/addons/ui_f_enyo/cfgMods.hpp
+++ @AchillesEnyoExpansion/addons/ui_f_enyo/cfgMods.hpp
@@ -11,13 +11,13 @@ class CfgMods
     actionName = "Website";
     action = "https://forums.bistudio.com";
 
-    description = "Ares augments the existing Zeus functionality, expanding the toolset and making it easy to create more compelling missions on the fly.<br />The Achilles Expansion (AE) includes Ares Mod with even more functionalites. Some functionalities only work with ACE mod, but those mods are not mandatory in order to run Ares (AE) properly.<br/>The Enyo Expansion focuses more on bringing back the functionalites from MCC Sandbox 4 with improved support and them being updated to newest Arma 3 standards. It uses elements from Achilles Expansion addon that improve the look and feel of Enyo Expansion.";
-    
-    overview = "Ares augments the existing Zeus functionality, expanding the toolset and making it easy to create more compelling missions on the fly.<br />The Achilles Expansion (AE) includes Ares Mod with even more functionalites. Some functionalities only work with ACE mod, but those mods are not mandatory in order to run Ares (AE) properly.<br/>The Enyo Expansion focuses more on bringing back the functionalites from MCC Sandbox 4 with improved support and them being updated to newest Arma 3 standards. It uses elements from Achilles Expansion addon that improve the look and feel of Enyo Expansion.";
+    description = "The Enyo Expansion focuses more on bringing back the functionalites from MCC Sandbox 4 with improved support and them being updated to newest Arma 3 standards. It uses elements from Achilles Expansion addon that improve the look and feel of Enyo Expansion.";
 
-    picture = "\enyo\data_f_enyo\icons\icon_enyo.paa";
+    overview = "The Enyo Expansion focuses more on bringing back the functionalites from MCC Sandbox 4 with improved support and them being updated to newest Arma 3 standards. It uses elements from Achilles Expansion addon that improve the look and feel of Enyo Expansion.";
+
+    picture = "\enyo\data_f_enyo\icons\icon_enyo_large.paa";
     logo = "\enyo\data_f_enyo\icons\icon_enyo.paa";
-    logoOver = "\enyo\data_f_enyo\icons\icon_enyo.paa";
+    logoOver = "\enyo\data_f_enyo\icons\icon_enyo_large.paa";
     logoSmall = "\enyo\data_f_enyo\icons\icon_enyo.paa";
   };
 };
